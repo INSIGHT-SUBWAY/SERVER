c7b87d37c728b75202ad3b619c55a7c22a29ac20
13155a1 <ppy040@gmail.com>
2023-11-24 14:05:56 +0900

    feat: 에러 메세지 수정

--- subway/views.py
+++ subway/views.py
@@ -22,7 +22,7 @@ def current_congestion_list(request):
     STATION_NAME = start_station
     INOUT_TAG = in_out_tag(start_station, end_station)
     if (INOUT_TAG == -1):
-        return Response("입력하신 역의 정보가 존재하지 않습니다.", status=status.HTTP_200_OK)
+        return JsonResponse({"error" : "입력하신 역의 정보가 존재하지 않습니다."}, status=status.HTTP_200_OK)
     SEOUL_KEY = "6e4a46554a70707933335467424375"
     SK_KEY = "ZWIH3CaKXp3ivs1nrevX5abFzWs9bZQpct4kwz1i"
 
@@ -32,13 +32,13 @@ def current_congestion_list(request):
     # 현재 들어오고 있는 열차 정보 받아오기
     CURRENT_TRAIN = train_code(STATION_NAME, CURRENT_TIME, INOUT_TAG, SEOUL_KEY)
     if (CURRENT_TRAIN == -1):
-        return Response("현재 들어오는 열차 정보가 없습니다.", status=status.HTTP_200_OK)
+        return JsonResponse({"error" : "현재 들어오는 열차 정보가 없습니다."}, status=status.HTTP_200_OK)
     
     # 그 열차에 대한 실시간 혼잡도 리스트 출력
     CONGESTION_LIST = train_number_to_current_congestion_list(CURRENT_TRAIN['TRAIN_CODE'], SK_KEY)
     
     if CONGESTION_LIST == -1:
-        return Response("현재 들어오는 열차의 혼잡도 정보가 없습니다.", status=status.HTTP_200_OK)
+        return JsonResponse({"error" : "현재 들어오는 열차의 혼잡도 정보가 없습니다."}, status=status.HTTP_200_OK)
     
     # DISCOMFORT_LEVEL 계산
     CONGESTION_SUM = 0
