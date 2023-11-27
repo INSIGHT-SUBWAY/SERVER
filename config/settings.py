ba55f430b82ed8b3cdb4b0c23fece7e1b41c9d16
13155a1 <ppy040@gmail.com>
2023-11-27 19:31:19 +0900

    fix: cors 에러 또또또 수정

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS 설정
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
 #     "http://127.0.0.1:8000",
