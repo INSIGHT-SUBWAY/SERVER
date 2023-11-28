6355817957537e05a06c1b18dc9878cf3599d622
13155a1 <ppy040@gmail.com>
2023-11-28 19:20:43 +0900

    fix: EC2 500 에러 해결

--- subway/urls.py
+++ subway/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('analyze/', analyze, name='analyze'),
-    path('xgboost/', xgboost, name='xgboost'),
+    # path('xgboost/', xgboost, name='xgboost'),
 ]
